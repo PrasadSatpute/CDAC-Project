fae998ef3232b3bb7ec9fabf1d36f772de1613af
PrasadSatpute <prasadsatpute000@gmail.com>
2021-10-12 06:33:41 +0530

    Update

---
+++ gymproject/src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div>
+            <ul>
+                <li><a href='Admin.html'>Admin</a></li>
+                <li><a href='User.html'>User</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home
